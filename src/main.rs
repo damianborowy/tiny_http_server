2adb04c2ad2c5f21702de12c4f66f5f8fd3b1589
Damian Borowy <damian.borowy@likims.com>
2023-03-04 17:27:07 +0100

    fix type errors

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
 use std::env;
 use std::fs::File;
-use tiny_http::{Server, Response, Request, Method};
-
-fn prepare_response(request: &Request) -> Response {
-    if request.method() == &Method::Get {
-        if request.url() == "/" {
-            return Response::from_file(File::open("index.html").unwrap());
-        }
-    }
-
-    if request.method() == &Method::Post {
-        if request.url() == "/echo" {
-            return Response::from_string("echoed");
-        }
-
-        return Response::from_string("not implemented");
-    }
-
-    return Response::from_string("not implemented");
-}
+use tiny_http::{Server, Response, Method};
 
 fn main() {
     let port = match env::var("PORT") {
@@ -29,7 +11,26 @@ fn main() {
     let server = Server::http(["0.0.0.0", &port].join(":")).unwrap();
 
     for request in server.incoming_requests() {
-        let response = prepare_response(&request);
-        let _ = request.respond(response);
+        if request.method() == &Method::Get {
+            if request.url() == "/" {
+                let _ = request.respond(Response::from_file(File::open("index.html").unwrap()));
+                continue;
+            }
+
+            let _ = request.respond(Response::from_string("not implemented"));
+            continue;
+        }
+
+        if request.method() == &Method::Post {
+            if request.url() == "/echo" {
+                let _ = request.respond(Response::from_string("echoed"));
+                continue;
+            }
+
+            let _ = request.respond(Response::from_string("not implemented"));
+            continue;
+        }
+
+        let _ = request.respond(Response::from_string("not implemented"));
     }
 }
